6e109a84907f126b05172281dba620846e7de775
David Williamson <daviwil2@users.noreply.github.com>
2021-03-06 17:39:28 +0400

    Edited placeholder text in api.js

--- api.js
+++ api.js
@@ -11,11 +11,7 @@ module.exports = [
 		description: 'Return list of known Espurna devices',
 		method: 'GET',
 		path: '/devices/',
-		fn: (args, callback) => {
-			// logic to get data from Homey.app.var and/or Homey.app.var.db (?)
-      // if true return callback(null, Homey.app.nestAccount.db.getAuth());
-			// else return callback('No nest account found');
-		},
+		fn: (args, callback) => {}
 	}
   */
 
